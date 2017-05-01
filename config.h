94f35b1f14268352cdc9e84f9676e0094e8b6339
cobra85ec <cobra85ec@gmail.com>
2017-04-30 21:45:41 -0400

    Update config.h

--- config.h
+++ config.h
@@ -1,5 +1,7 @@
 //#define DEBUG_PRINT
 
 #define MQTT_SERVER "192.168.1.30"
+#define MQTT_USER "192.168.1.30"
+#define MQTT_PASSWORD "192.168.1.30"
 #define MQTT_TOPIC "house/room1/pir"
 #define  CLIENT_ID "room1"
